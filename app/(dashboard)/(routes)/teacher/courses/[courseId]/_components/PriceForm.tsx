178eb36877fcad8c121c8b3094fba38a2059a1ec
Antonov-S <anto1nov1@gmail.com>
2023-09-27 19:13:41 +0300

    Adding a Course Price Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/PriceForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/PriceForm.tsx
@@ -13,19 +13,19 @@ import { Course } from "@prisma/client";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { Combobox } from "@/components/ui/combobox";
+import { Input } from "@/components/ui/input";
+import { formatPrice } from "@/lib/format";
 
-interface CategoryFormProps {
+interface PriceFormProps {
   initialData: Course;
   courseId: string;
-  options: { label: string; value: string }[];
 }
 
 const formSchema = z.object({
-  categoryId: z.string().min(1)
+  price: z.coerce.number()
 });
 
-export const CategoryForm = ({ initialData, courseId, options }: CategoryFormProps) => {
+export const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const toggleEdit = () => setIsEditing(current => !current);
@@ -35,7 +35,7 @@ export const CategoryForm = ({ initialData, courseId, options }: CategoryFormPro
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      categoryId: initialData?.categoryId || ""
+      price: initialData?.price || undefined
     }
   });
 
@@ -52,34 +52,32 @@ export const CategoryForm = ({ initialData, courseId, options }: CategoryFormPro
     }
   };
 
-  const selectedOption = options.find(option => option.value === initialData.categoryId);
-
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Course category
+        Course price
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing ? (
             <>Cancel</>
           ) : (
             <>
               <Pencil className="h-4 w-4 mr-2" />
-              Edit category
+              Edit price
             </>
           )}
         </Button>
       </div>
-      {!isEditing && <p className={cn("text-sm mt-2", !initialData.categoryId && "text-slate-500 italic")}>{selectedOption?.label || "No category"}</p>}
+      {!isEditing && <p className={cn("text-sm mt-2", !initialData.price && "text-slate-500 italic")}>{initialData.price ? formatPrice(initialData.price) : "No price"}</p>}
       {isEditing && (
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4">
             <FormField
               control={form.control}
-              name="categoryId"
+              name="price"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Combobox options={...options} {...field} />
+                    <Input type="number" step="0.01" disabled={isSubmitting} placeholder="Set a price for your course" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
