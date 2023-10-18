7bfa7274933561785ef6f975f2c037431429e6f4
Antonov-S <anto1nov1@gmail.com>
2023-10-18 22:36:50 +0300

    Adding a Chapter Progress Functionality

--- app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
+++ app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
@@ -1,8 +1,10 @@
 import { auth } from "@clerk/nextjs";
+import { Chapter, Course, UserProgress } from "@prisma/client";
 import { redirect } from "next/navigation";
 
-import { Chapter, Course, UserProgress } from "@prisma/client";
 import { db } from "@/lib/db";
+import { CourseProgress } from "@/components/CourseProgress";
+
 import { CourseSidebarItem } from "./CourseSidebarItem";
 
 interface CourseSidebarProps {
@@ -18,7 +20,7 @@ export const CourseSidebar = async ({ course, progressCount }: CourseSidebarProp
   const { userId } = auth();
 
   if (!userId) {
-    redirect("/");
+    return redirect("/");
   }
 
   const purchase = await db.purchase.findUnique({
@@ -34,7 +36,11 @@ export const CourseSidebar = async ({ course, progressCount }: CourseSidebarProp
     <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
       <div className="p-8 flex flex-col border-b">
         <h1 className="font-semibold">{course.title}</h1>
-        {/* TODO: Check purchase and add progress */}
+        {purchase && (
+          <div className="mt-10">
+            <CourseProgress variant="success" value={progressCount} />
+          </div>
+        )}
       </div>
       <div className="flex flex-col w-full">
         {course.chapters.map(chapter => (
